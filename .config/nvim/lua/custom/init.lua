da689c7b08d08dcf0d0a272edf161839701e1877
oliverdding <oliverdding@outlook.com>
2022-01-13 19:59:46 +0800

    update

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,3 +3,27 @@ local map = require("core.utils").map
 
 map("n", "<leader>cc", ":Telescope <CR>")
 map("n", "<leader>q", ":q <CR>")
+
+-- PLUGINS
+local customPlugins = require "core.customPlugins"
+
+customPlugins.add(function(use)
+    -- faster filetype
+    use {"nathom/filetype.nvim"}
+
+    -- smooth scroll
+    use {
+        "karb94/neoscroll.nvim",
+        opt = true,
+        config = function() require("neoscroll").setup() end,
+
+        -- lazy loading
+        setup = function()
+            require("core.utils").packer_lazy_load "neoscroll.nvim"
+        end
+    }
+end)
+
+-- SETTINGS
+-- load filetype.nvim instead of filetype.vim
+vim.g.did_load_filetypes = 1
