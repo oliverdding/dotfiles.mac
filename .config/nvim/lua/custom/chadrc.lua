2ba705379261cf2d1d2a6bc6fe937d5184285744
oliverdding <oliverdding@outlook.com>
2022-01-26 15:03:29 +0800

    arrange

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -9,41 +9,20 @@ M.plugins = {
     options = {
         statusline = {
             style = "arrow" -- default, round , slant , block , arrow
-        },
-        default_plugin_config_replace = {
-            nvim_treesitter = {
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "cmake",
-                    "cpp",
-                    "dockerfile",
-                    "go",
-                    "java",
-                    "json5",
-                    "latex",
-                    "llvm",
-                    "lua",
-                    "make",
-                    "python",
-                    "regex",
-                    "rust",
-                    "scala",
-                    "toml",
-                    "yaml",
-                }
-            },
-            nvim_tree = {
-                view = {
-                    side = 'right',
-                    auto_resize = true,
-                  },
-                  trash = {
-                    cmd = "rmtrash",
-                    require_confirm = true,
-                  },
+        }
+    },
+    default_plugin_config_replace = {
+        nvim_treesitter = {
+            ensure_installed = {
+                "bash", "c", "cmake", "cpp", "dockerfile", "go", "java",
+                "json5", "latex", "llvm", "lua", "make", "python", "regex",
+                "rust", "scala", "toml", "yaml"
             }
         },
+        nvim_tree = {
+            view = {side = 'right', auto_resize = true},
+            trash = {cmd = "rmtrash", require_confirm = true}
+        }
     }
 }
 
