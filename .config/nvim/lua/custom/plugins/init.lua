2d6cb94d7c87af6ab20859c1f5f2492de860c232
oliverdding <oliverdding@outlook.com>
2022-01-27 16:27:32 +0800

    add which-key; add dap related stuff

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -9,4 +9,25 @@ customPlugins.add(function(use)
         config = function() require("stabilize").setup() end
     }
 
+    -- dap
+    use {
+        "folke/which-key.nvim",
+        config = function() require "custom.plugins.whichkey" end
+    }
+
+    use {"mfussenegger/nvim-dap", after = "nvim-lspconfig"}
+
+    use {
+        "leoluz/nvim-dap-go",
+        opt = true,
+        after = "nvim-dap",
+        config = function() require("dap-go").setup() end
+    }
+
+    use {
+        "mfussenegger/nvim-dap-python",
+        opt = true,
+        after = "nvim-dap",
+        config = function() require("dap-python").setup("python") end
+    }
 end)
