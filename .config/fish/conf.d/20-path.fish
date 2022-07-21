9577fbb375c040ea8592e925b0fc45a07f8c6b7a
oliver ding <oliverdding@outlook.com>
2022-07-21 17:04:29 +0800

    last update. change to oliverdding/dotfiles

--- .config/fish/conf.d/20-path.fish
+++ .config/fish/conf.d/20-path.fish
@@ -3,6 +3,6 @@ fish_add_path -gP /opt/local/bin
 fish_add_path -g $HOME/.local/bin
 fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $HOME/.local/share/go/bin
-fish_add_path -g $HOME/Library/Python/3.10/bin
+fish_add_path -g (ls -1dD $HOME/Library/Python/* 2>/dev/null | sed 's#$#/bin#')
 fish_add_path -g $HOME/.npm/bin
 fish_add_path -g $HOME/.krew/bin
--- .config/fish/conf.d/40-work.fish
+++ .config/fish/conf.d/40-work.fish
@@ -1,1 +1,3 @@
 set -gx QAPM_HOME "$HOME/Developer/QAPM"
+set -gx INTRANET_PROXY_JUMP_SERVERS 'devcloud'
+set -gx TMF_PROXY_JUMP_SERVERS 'devcloud,mixed-main-22'
